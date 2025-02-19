c2a13a037fcc169fd3f049f06d23a111c2c7fdf6
DSHEP2024 <djshepherd23@students.desu.edu>
2025-02-19 12:26:41 -0500

    finished

--- src/main/java/Recitation4.java
+++ src/main/java/Recitation4.java
@@ -19,11 +19,35 @@ public class Recitation4 {
     public static String explode(String word){
         StringBuilder sb = new StringBuilder();
 
-        // TODO: replace this line with your code
+            int pos;
+            // Loop through the string
 
+            for (pos = 0; pos < word.length(); pos++) {
+                char currentChar = word.charAt(pos);
+                if (Character.isDigit(currentChar)) {
+                    sb.repeat(currentChar, (int)(currentChar - '0'));
+                } else if (Character.isLetter(currentChar)) {
+                    sb.repeat(currentChar, (pos + 1));
+                } else {
+
+                    // Stop expansion and append
+                    String subString = word. substring(0, pos);
+                    sb.append(subString);
+                }
+            }
         return sb.toString();
     }
 }
 /*
-TODO: Replace this line with your algorithm
+1. Assume we have a variable called pos
+2. Assume we have a variable called sb
+3. start with the first character
+4. if the character is a digit, then
+     a. duplicate the character based on its value
+ 5. else if the character is a letter, then duplicate by its position
+ 6. else if the character is not (a letter and not a digit), then append string up to but not including the character
+ 7 . get the next character
+ 8. if there is another character then goto 4
+ 9. Done
+
 */
